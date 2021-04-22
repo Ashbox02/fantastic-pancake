81abc211fa4f8ec7a318545c3b61b8fcf299f1cc
Ashbox02 <harvunit@gmail.com>
2021-04-21 19:38:36 -0700

    added color time again

--- index.js
+++ index.js
@@ -1,28 +1,10 @@
-const button=document.getElementById("button-main");
+const button=document.getElementById("color-button");
 console.log(button)
-let x = 0;
+
 button.addEventListener("click", () => {
-
-    if (x == 0) {
-    let red = 30;
-    let green = 30;
-    let blue = 30;
+    let red = Math.floor(Math.random() * 255);
+    let green = Math.floor(Math.random()* 255);
+    let blue = Math.floor(Math.random()* 255);
     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-    
-    document.getElementById("header-welcome").style.color = "#ffa64f";
-    document.getElementById("paragraph-1").style.color = "#ffa64f";
-
-    }
-    else if (x == 1) {
-    let red = 255;
-    let green = 255;
-    let blue = 255;
-    document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-    
-    document.getElementById("header-welcome").style.color = "#000000";
-    document.getElementById("paragraph-1").style.color = "#000000";
-    
-    }
-    x++
-    if (x>1) x=0;
 })
+
